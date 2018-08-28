7bce62d1b1f38095b95ac52bc146e0bf6a69a527
Alan Beech <alan@beech.me.uk>
2018-08-28 19:14:49 +0100

    added angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import {MatIconRegistry} from '@angular/material';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'board-manager';
+
+  constructor() {
+
+  }
 }
