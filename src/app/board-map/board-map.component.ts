997fffa609b9ffded224fb715e194ca4168004ff
Alan Beech <alan@beech.me.uk>
2018-09-26 19:30:30 +0100

    added map points

--- src/app/board-map/board-map.component.ts
+++ src/app/board-map/board-map.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {AccountService} from '../account.service';
+import {MatDialog, MatTableDataSource} from '@angular/material';
+import {NotificationType} from '../services/notification-type.enum';
+import {Board} from '../boards/boards.component';
+import {BoardsService} from '../boards.service';
+import {ViewBoardComponent} from '../view-board/view-board.component';
 
 @Component({
   selector: 'app-board-map',
@@ -11,9 +17,37 @@ export class BoardMapComponent implements OnInit {
   lng: number = 7.809007;
   lat2: number = 52.678418;
   lng2: number = 6.809007;
-  constructor() { }
+  boards: Array<Board>;
+  constructor(private accountService: AccountService, private boardsService: BoardsService, public dialog: MatDialog) { }
+
+  getBoards(boardType: number) {
+
+    // this.isLoggedIn = this.accountService.getKey() !== '' && this.accountService.getKey() != null ;
+
+    this.boardsService.getBoards(-1).subscribe((data:  Array<Board>) => {
+      this.boards  =  data;
+      // this.dataSource = new MatTableDataSource(this.boards);
+      // this.applyFilter(this.filterInput.value);
+    }, (error) => {
+      // this.notificationService.showNotification(`Sorry. We are unable to get the boards at this time. Please try later`, NotificationType.Error);
+    });
+  }
+
+  viewBoard(board: Board) {
+    console.log(board);
+    const dialogRef = this.dialog.open(ViewBoardComponent, {
+      width: '600px',
+      data: board
+    });
+
+    dialogRef.afterClosed().subscribe((edited: boolean) => { });
+  }
 
   ngOnInit() {
+    //this.accountService.loggedIn.subscribe((loggedIn) => {
+      // this.isLoggedIn = loggedIn;
+      this.getBoards(0);
+    //});
 
   }
 
